99ec9cffc5fc78e142da03d5d8b422ad71aa52a2
Shiyu <lishiyu@LiShiyus-MacBook-Pro.local>
2016-02-05 20:04:16 +0100

    sql info

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
-  //user     : 'dbuser',
-  //password : 's3kreee7'
+  user     : 'root',
+  password : 'sqlpass'
 });
 module.exports = connection;
